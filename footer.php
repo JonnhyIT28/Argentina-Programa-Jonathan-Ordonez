d6a0a7012a79f5f6874104df11e008c60620131d
JonnhyIT28 <123526456+JonnhyIT28@users.noreply.github.com>
2023-02-14 10:54:56 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer" id="contacto">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="h1_footer">Alberto Camus</h1>
+            <p><a class="a_footer" href="miemail@gmail.com">esteemailesabsurdo@gmail.com</a></p>
+            <p>+549999999999</p>
+            <p>Lourmarin, Provenza-Alpes-Costa Azul, Francia</p>
+        </div>
+    </div>
+</footer>
---
+++ javascript.js
@@ -0,0 +1,4 @@
+function mostrarQuote() {
+    document.getElementById("quote").innerHTML = "La única manera de lidiar con este mundo sin libertad es volverte tan absolutamente libre que tu mera existencia sea un acto de rebelión";
+}
+
